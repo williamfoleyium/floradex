d26a4ead4da30a4d7daa8ca238be42cc06f98cd4
WilliamFoley <CaptainTrips1500@proton.me>
2025-04-26 09:22:26 -0400

    added jest and vite packages for TDD

---
+++ floradex-frontend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
+    }
+  };
